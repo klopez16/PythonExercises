c6922aab7195dbbca45e512f1fcf605bcd9c2204
klopez16 <klopezb16@outlook.com>
2026-02-05 21:42:55 -0500

    Python Basics Day 1

--- Exercise1.py
+++ Exercise1.py
@@ -1,1 +1,53 @@
-print('Hello World')
+#Python Basics
+
+#Message = 'Hello World'
+
+from calendar import different_locale
+
+
+Message = 'Hello World'
+print(Message)
+
+#Variables
+nombre = 'Pedro' #string - texto
+edad = 30 #int - numeros enteros
+precio = 19.90 #float - numeros decimales
+esta_activo = True #boolean - condicion de verdadero o falso
+
+print (nombre)
+print (edad)
+print (precio)
+print (esta_activo)
+
+#Operadores aritmeticos
+suma = 10 + 5
+resta = 10 + 5
+multiplicacion = 10 * 5
+division = 10 / 5
+division_entera = 10 // 3
+modulo = 10 % 5 #residuo de la division por ejemplo 10 / 5 = 2 y residuo 0 pero 10 % 3 = 1 y residuo 1
+potencia = 10 ** 2
+
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
+print (division_entera)
+print (modulo)
+print (potencia)
+
+
+#Operadores de comparacion
+mayor = 10 > 5
+menor = 10 < 5
+igual = 10 == 5
+diferente = 10 != 5
+mayor_igual = 10 >= 5
+menor_igual = 10 <= 5
+
+print (mayor)
+print (menor)
+print (igual)
+print (diferente)
+print (mayor_igual)
+print (menor_igual)
